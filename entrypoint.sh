b77d56108b9ce6cee58f9819b037111cf6ffb872
Behzad Ghaffarian <behzad.ghaffarian@gmail.com>
2023-02-02 15:25:32 +0100

    remove extra features

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_FILE" ]
+if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
   echo "Source file must be defined"
   return 1
@@ -27,37 +27,11 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_RENAME" ]
-then
-  echo "Setting new filename: ${INPUT_RENAME}"
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
-else
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-fi
-
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-if [ -z "$INPUT_USE_RSYNC" ]
-then
-  cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
-else
-  echo "rsync mode detected"
-  rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
-fi
-
+# mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
+rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER
+cp -a $INPUT_SOURCE_FOLDER/* $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cd "$CLONE_DIR"
-
-if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
-then
-  echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
-  git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
-  OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
-fi
-
-if [ -z "$INPUT_COMMIT_MESSAGE" ]
-then
-  INPUT_COMMIT_MESSAGE="Update from https://$INPUT_GIT_SERVER/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}"
-fi
 
 echo "Adding git commit"
 git add .
